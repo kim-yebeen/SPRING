3a6ef700c3f80f5406a4310e64dd6bc80b13b0cc
kim-yebeen <kimyebeen125@gmail.com>
2024-01-29 02:20:36 +0900

    웹MVC 개발

--- src/main/java/kim/kimspring/Controller/MemberController.java
+++ src/main/java/kim/kimspring/Controller/MemberController.java
@@ -1,9 +1,16 @@
 package kim.kimspring.Controller;
 
+import kim.kimspring.domain.Member;
 import kim.kimspring.repository.MemberRepository;
 import kim.kimspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
 
 //spring 컨테이너가 스프링 창에 뜰 때, 멤버 컨트롤러 생성을 해서 넣어논다.
 
@@ -23,5 +30,28 @@ public class MemberController {
     //MemberService의 생성자에 @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/kim/kimspring/Controller/MemberForm.java
@@ -0,0 +1,13 @@
+package kim.kimspring.Controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
