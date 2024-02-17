bbaecdf03904ecb9794af64ad01c13709615c188
kim-yebeen <kimyebeen125@gmail.com>
2024-02-17 23:55:37 +0900

    Spring DB 접근기술 - JPA

--- src/main/java/kim/kimspring/domain/Member.java
+++ src/main/java/kim/kimspring/domain/Member.java
@@ -1,6 +1,14 @@
 package kim.kimspring.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
--- src/main/java/kim/kimspring/repository/JpaMemerRepository.java
+++ src/main/java/kim/kimspring/repository/JpaMemerRepository.java
@@ -1,2 +1,36 @@
-package kim.kimspring.repository;public class JpaMemerRepository {
+package kim.kimspring.repository;
+
+import jakarta.persistence.EntityManager;
+import kim.kimspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemerRepository implements MemberRepository{
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em){
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
 }
